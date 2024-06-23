613bb5fbc3330c26aec65eca358313f3bacfbe1f
Orivaldo Santana <orivaldo@gmail.com>
2024-06-23 16:16:21 -0300

    Melhorando o código de cadastro

--- src/app/lib/action.ts
+++ src/app/lib/action.ts
@@ -1,32 +1,30 @@
-'use server'
-import { PrismaClient } from '@prisma/client'
+"use server";
+import { PrismaClient } from "@prisma/client";
 
 export const addTeste = async (formData: FormData) => {
-  let name = formData.get("nome")
-  let email = formData.get("email")
-  console.log("Form: " + name + " " + email)
-}
+  let name = formData.get("nome");
+  let email = formData.get("email");
+  console.log("Form: " + name + " " + email);
+};
 
 export const addUser = async (formData: FormData) => {
-  let name = formData.get('nome')
-  let email = formData.get('email')
+  let name = formData.get("nome");
+  let email = formData.get("email");
 
-  console.log(`addUser ${name} ${email}`)
+  console.log(`addUser ${name} ${email}`);
 
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient();
 
   try {
     await prisma.user.create({
       data: {
-        name: name.toString(),
-        email: email.toString(),
-
-      }
-    })
-   
+        name: name ? name.toString() : "",
+        email: email ? email.toString() : "",
+      },
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   } finally {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   }
-}
+};

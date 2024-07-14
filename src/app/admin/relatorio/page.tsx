0c41c53e296bc5b5b8774b977c8a85408bbbe819
Orivaldo Santana Jr <orivaldo@gmail.com>
2024-07-14 20:48:17 -0300

    Update page.tsx

--- src/app/admin/relatorio/page.tsx
+++ src/app/admin/relatorio/page.tsx
@@ -1,18 +1,19 @@
-import { getUserData } from "@/app/lib/data";
-import GenericPage from "@/ui/genericpage";
+import GenericPage from '@/ui/genericpage'
+import { getUserData } from '@/app/lib/data'
 
 export default async function RelatorioPage() {
-  const users = await getUserData();
-
+  const users = await getUserData()
   return (
-    <GenericPage title={"Relatórios"}>
-      {users !== undefined && (
+    <GenericPage title={'Relatórios'}>
+      {users !== null && (
         <div>
-          {users.map((user) => (
-            <div key={user.name}>{user.name}</div>
+          {users.map(user => (
+            <div key={user.id}>
+              {user.name} - {user.email}
+            </div>
           ))}
         </div>
       )}
     </GenericPage>
-  );
+  )
 }

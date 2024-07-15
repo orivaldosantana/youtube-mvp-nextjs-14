c11c066d798e476917e6dfd2470a2080b93aa5f7
Orivaldo Santana Jr <orivaldo@gmail.com>
2024-07-14 22:08:59 -0300

    Mostrar uma mensagem de sucesso ou erro ao gravar um dado

--- src/app/lib/action.ts
+++ src/app/lib/action.ts
@@ -1,30 +1,40 @@
-"use server";
-import { PrismaClient } from "@prisma/client";
+'use server'
+import { PrismaClient } from '@prisma/client'
 
 export const addTeste = async (formData: FormData) => {
-  let name = formData.get("nome");
-  let email = formData.get("email");
-  console.log("Form: " + name + " " + email);
-};
+  let name = formData.get('nome')
+  let email = formData.get('email')
+  console.log('Form: ' + name + ' ' + email)
+}
 
-export const addUser = async (formData: FormData) => {
-  let name = formData.get("nome");
-  let email = formData.get("email");
+export const addUser = async (
+  prevState: { message: string },
+  formData: FormData
+) => {
+  let name = formData.get('nome')
+  let email = formData.get('email')
+  let returnMessage
 
-  console.log(`addUser ${name} ${email}`);
+  console.log(`addUser ${name} ${email}`)
 
-  const prisma = new PrismaClient();
+  const prisma = new PrismaClient()
 
   try {
     await prisma.user.create({
       data: {
-        name: name ? name.toString() : "",
-        email: email ? email.toString() : "",
-      },
-    });
+        name: name ? name.toString() : '',
+        email: email ? email.toString() : ''
+      }
+    })
+    returnMessage = {
+      message: 'Usuário adicionado com sucesso!',
+      type: 'success'
+    }
   } catch (error) {
-    console.error(error);
+    console.error(error)
+    returnMessage = { message: 'Erro ao adicionar usuário!', type: 'error' }
   } finally {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
+    return returnMessage
   }
-};
+}
--- src/ui/forms/colaborador.jsx
+++ src/ui/forms/colaborador.jsx
@@ -1,18 +1,35 @@
-import { addUser } from "@/app/lib/action"
-import TextField from "@mui/material/TextField"
-import Box from "@mui/material/Box"
-import Button from "@mui/material/Button" // Add this line to import the Button component
-import { Typography } from "@mui/material"
+'use client'
+import { addUser } from '@/app/lib/action'
+import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button' // Add this line to import the Button component
+import { IconButton, Typography, Alert, Collapse } from '@mui/material'
+import { Close } from '@mui/icons-material'
+import { useFormState } from 'react-dom'
+import { useEffect, useState } from 'react' // Add this line to import the useEffect hook
+
+const initialState = {
+  message: ''
+}
 
 export default function FormColaborador({ title }) {
+  const [state, formAddUserAction] = useFormState(addUser, initialState)
+  const [openAlert, setOpenAlert] = useState(false)
+
+  useEffect(() => {
+    console.log(state)
+    //setOpenAlert(true)
+    if (state?.message) setOpenAlert(true)
+  }, [state])
+
   return (
     <Box
-      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
       <Typography variant="h4" sx={{ marginBottom: 2 }}>
         {title}
       </Typography>
-      <form action={addUser}>
+      <form action={formAddUserAction}>
         <TextField
           name="nome"
           label="Nome"
@@ -32,6 +49,18 @@ export default function FormColaborador({ title }) {
         <Button variant="contained" type="submit" fullWidth>
           Cadastrar Colaborador
         </Button>
+        <Collapse in={openAlert}>
+          <Alert
+            severity={state?.type}
+            action={
+              <IconButton onClick={() => setOpenAlert(false)}>
+                <Close />
+              </IconButton>
+            }
+          >
+            {state?.message}
+          </Alert>
+        </Collapse>
       </form>
     </Box>
   )
